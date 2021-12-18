8ff2520d472574440fb24d4ac7116a7402c08493
F1oop <80451125+F1oop@users.noreply.github.com>
2021-12-18 12:12:03 +0300

    added turning

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+
 class Snake:
 
     def __init__(self):
@@ -20,3 +21,19 @@ class Snake:
         for seg_num in range(len(self.segments) - 1, 0, -1):
             self.segments[seg_num].goto(x=self.segments[seg_num - 1].xcor(), y=self.segments[seg_num - 1].ycor())
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)

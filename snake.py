0b11ebf0df59dd3a738df1b52e426b2ef88bcc27
F1oop <80451125+F1oop@users.noreply.github.com>
2021-12-18 11:55:04 +0300

    refactor

--- snake.py
+++ snake.py
@@ -1,19 +1,22 @@
 from turtle import Turtle
-
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 class Snake:
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-    segments = []
 
-    for position in starting_positions:
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        segments.append(new_segment)
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
     def move(self):
-
         for seg_num in range(len(self.segments) - 1, 0, -1):
             self.segments[seg_num].goto(x=self.segments[seg_num - 1].xcor(), y=self.segments[seg_num - 1].ycor())
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)

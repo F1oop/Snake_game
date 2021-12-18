e7ae5a697ef01c9fc7cb14a48aac03552e64b70b
F1oop <80451125+F1oop@users.noreply.github.com>
2021-12-18 13:35:55 +0300

    scoreboard added

---
+++ scoreboard.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.score = 0
+        self.goto(0, 260)
+        self.color("White")
+        self.my_score()
+        self.hideturtle()
+
+    def my_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=('Courier', 20, 'normal'))
+        self.score += 1

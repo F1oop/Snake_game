bce29d8a197b09ec60589df748fb811a17de7740
F1oop <80451125+F1oop@users.noreply.github.com>
2021-12-18 10:53:40 +0300

    code as Angela

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
-x = 0
-y = 0
 
-for segment in range(0, 3):
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
-    new_segment.goto(x=x, y=y)
-    x -= 20
-    y = y
+    new_segment.goto(position)
 
 screen.exitonclick()

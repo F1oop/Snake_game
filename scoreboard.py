daa8fd0242dd26cfc4a26bdfdf5c5a48e5d30e21
F1oop <80451125+F1oop@users.noreply.github.com>
2021-12-19 14:29:51 +0300

    high score table

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.score = 0
+        self.high_score = 0
         self.goto(0, 260)
         self.color("White")
         self.my_score()
@@ -14,9 +15,14 @@ class Scoreboard(Turtle):
 
     def my_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=('Courier', 20, 'normal'))
+        self.write(f"Score: {self.score}, High score: {self.high_score}", align="center", font=('Courier', 20, 'normal'))
         self.score += 1
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align="center", font=('Courier', 40, 'normal'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", align="center", font=('Courier', 40, 'normal'))
